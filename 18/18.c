dd50a0a2a3e115698bff316ff384b27b7a579de0
Zandaka <akira.s.20060314@gmail.com>
2024-11-19 20:06:58 +0900

    try commit

---
+++ 18/18.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+#define ndat 20
+#define precision 0.1
+int main(void)
+{
+     FILE*fp;
+     float t[ndat],x[ndat],s[ndat],y[ndat],exp_y[ndat];
+     float para_v,para_x0;
+     int ih;
+
+     if()
+        }
