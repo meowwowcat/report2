8736397fcf23ed02bed089932d73c102fefb4c6a
Zandaka <akira.s.20060314@gmail.com>
2024-11-19 20:16:39 +0900

    try commit

--- 18/18.c
+++ 18/18.c
@@ -9,5 +9,19 @@ int main(void)
      float para_v,para_x0;
      int ih;
 
-     if()
-        }
+     if((fp=fopen("u.dat","r"))!=NULL){
+             for(ih=0;ih<ndat;ih++){
+                     fscanf(fp,"%f %f %f %f %f",&t[ih],&x[ih],&y[ih],&exp_y[ih],&s[ih]);
+                     s[ih]=precision;
+                     }
+              fclose(fp);
+
+             }
+             else{
+                  printf("file open error!\n");
+                  return -1;
+                  }
+             float SUM_T2=0,SUM_T=0,SUM_X=0,SUM_TX=0,SUM_1=0;
+             int i;
+             for()
+}
