7148817257d4e92af6df883b555a45fda0d92fa5
Zandaka <akira.s.20060314@gmail.com>
2024-11-19 20:34:47 +0900

    -h
    a
    
    q
    quit()
    
    hvolvg

--- 18/18.c
+++ 18/18.c
@@ -23,5 +23,26 @@ int main(void)
                   }
              float SUM_T2=0,SUM_T=0,SUM_X=0,SUM_TX=0,SUM_1=0;
              int i;
-             for()
+             for(i=0;i<ndat;i++){
+                        SUM_T2  += t[i] * t[i]/(s[i] * s[i]);
+                        SUM_T   += t[i] / ( s[i] * s[i]);
+                        SUM_X   += x[i] / ( s[i] * s[i]);
+                        SUM_TX  += t[i] * x[i] / ( s[i] * s[i]);
+                        SUM_1   += 1.0 / ( s[i] * s[i]);
+                     }
+              para_v = (SUM_TX * SUM_1 - SUM_T * SUM_X) / (SUM_T2 * SUM_1 - SUM_T * SUM_T);
+              para_x0 = (SUM_T2 * SUM_X - SUM_TX * SUM_T) / (SUM_T2 * SUM_1 - SUM_T * SUM_T);
+
+              fp=fopen("output.txt","w");
+              if( fp != NULL){
+                      fprintf(fp,"v=%f , x0=%f\n",para_v,para_x0);
+                      fclose(fp);
+                      printf("書き込みおｋ\n");
+                      }
+             else{
+                      printf("書き込みだめ\n");
+                      return -1;
+                     } 
+
+             return 0;
 }
